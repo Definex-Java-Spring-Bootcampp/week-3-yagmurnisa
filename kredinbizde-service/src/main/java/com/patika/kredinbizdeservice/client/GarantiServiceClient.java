7193dfa4604f54a8c5185970f6dece956d32faa3
yagmurnisa <ymr.nisa@hotmail.com>
2024-04-01 16:14:11 +0300

    added missing files

--- kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/client/GarantiServiceClient.java
+++ kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/client/GarantiServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.patika.kredinbizdeservice.client.dto.request.GarantiApplicationRequest;
 import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
 
-@FeignClient(value = "garanti-service", url = "localhost:8082")
+@FeignClient(value = "garanti-service", url = "localhost:8083")
 public interface GarantiServiceClient {
 
 	@PostMapping("api/garanti/v1/applications")
